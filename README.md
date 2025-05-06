6375d9045b668833eb047b3214f8abf2877d5d0c
sreeramraghu04 <sreeramraghu04@gmail.com>
2025-05-06 11:50:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Cat-Facts App
+
+A simple and fun React application that fetches random cat facts from an external API using **Axios**. Styled beautifully using **Tailwind CSS**.
+
+## ✨ Features
+
+- Fetches a random cat fact with the click of a button.
+- Uses Axios for API requests.
+- React functional components and hooks (`useState`) for state management.
+- Clean and modern UI using Tailwind CSS.
+
+## 🔧 Tech Stack
+
+- ⚛️ React (Functional Components)
+- 📦 Axios
+- 🎨 Tailwind CSS
+
+## Screenshots
+
+- Starting Page
+  ![alt text](startingpage-catfact.png)
+
+- After Clicking the button
+  ![alt text](afterclickingpage.png)
+
+## Demo Link
+
+[live Demo]()
---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [catFact, setCatFact] = useState(
+    "Click the button to get something new about cats!"
+  );
+
+  const FetchData = async () => {
+    const { data } = await axios.get("https://catfact.ninja/fact");
+    setCatFact(data.fact);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-cyan-100 p-4">
+      <div className="bg-cyan-200 shadow-md rounded-md p-25 max-w-md w-full text-center">
+        <h1 className="text-3xl font-bold text-teal-500 mb-10 font-mono underline">
+          Cat-Facts
+        </h1>
+
+        <button
+          onClick={FetchData}
+          className="bg-teal-500 hover:bg-teal-700 text-white font-semibold py-3 px-6 rounded-md shadow-md mb-4 cursor-pointer"
+        >
+          Get a Cat-Fact
+        </button>
+
+        <div className="text-gray-500 text-lg font-serif mt-4">{catFact}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
