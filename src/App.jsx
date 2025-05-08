124dd4427e2070e09e009f452aa554a5105df635
sreeramraghu04 <sreeramraghu04@gmail.com>
2025-05-08 10:29:22 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,19 @@ function App() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-cyan-100 p-4">
-      <div className="bg-cyan-200 shadow-md rounded-md p-25 max-w-md w-full text-center">
-        <h1 className="text-3xl font-bold text-teal-500 mb-10 font-mono underline">
+      <div className="bg-cyan-200 shadow-md rounded-md p-10 max-w-md w-full text-center">
+        <h1 className="lg:text-4xl text-xl font-bold text-teal-500 mb-10 font-mono underline">
           Cat-Facts
         </h1>
 
         <button
           onClick={FetchData}
-          className="bg-teal-500 hover:bg-teal-700 text-white font-semibold py-3 px-6 rounded-md shadow-md mb-4 cursor-pointer"
+          className="lg:text-2xl sm:text-xl bg-teal-500 hover:bg-teal-700 text-white font-semibold py-3 px-5 rounded-md shadow-md mb-4 cursor-pointer"
         >
           Get a Cat-Fact
         </button>
 
-        <div className="text-gray-500 text-lg font-serif mt-4">{catFact}</div>
+        <div className="text-gray-500 text-lg font-serif mt-4 mx-auto w-[250px] text-center">{catFact}</div>
       </div>
     </div>
   );
